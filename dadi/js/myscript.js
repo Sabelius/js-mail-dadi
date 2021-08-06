3910772d18e03706d406cadff77378323ada53d1
Sabelius <sbebby92@virgilio.it>
2021-08-06 14:59:12 +0200

    versione finale mail+dadi

---
+++ dadi/js/myscript.js
@@ -0,0 +1,27 @@
+let lancioGiocatore;
+let lancioComputer;
+let vincitore;
+
+console.log("implementazione variabili");
+
+
+lancioGiocatore = parseInt((6 * Math.random()) + 1);
+lancioComputer = parseInt((6 * Math.random()) + 1);
+
+console.log("definizione numero random");
+
+if(lancioGiocatore > lancioComputer){
+    vincitore = "Giocatore";
+}else if(lancioGiocatore = lancioComputer){
+    vincitore = "Riprovare";
+}else{
+    vincitore = "Computer";
+}
+
+console.log("implementazione condizioni");
+
+document.getElementById("lanciogiocatore").innerHTML = lancioGiocatore;
+document.getElementById("lanciocomputer").innerHTML = lancioComputer;
+document.getElementById("vincitore").innerHTML = vincitore;
+
+console.log("definizione elementi per id");
